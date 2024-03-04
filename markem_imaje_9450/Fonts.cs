e24b2bd124aa03f78fd1184506b09ae7a229db9c
Yaron Binder <75532643+YaronBinder@users.noreply.github.com>
2024-03-04 15:26:33 +0200

    Update Fonts.cs

--- markem_imaje_9450/Fonts.cs
+++ markem_imaje_9450/Fonts.cs
@@ -5,37 +5,37 @@
         /// <summary>
         /// Latin font Height: 32 | Width: 22
         /// </summary>
-        public static readonly Font Latin_32x22 = new(){ Height = 0x32, Dimensions = [0x01, 0x00] };
+        public static readonly Font Latin_32x22 = new(){ Height = 32, Dimensions = [0x01, 0x00] };
 
         /// <summary>
-        /// Latin font Height: 32 | Width: 22
+        /// Latin font Height: 5 | Width: 6
         /// </summary>
-        public static readonly Font Latin_5x6   = new(){ Height = 0x05, Dimensions = [0x01, 0x1A] };
+        public static readonly Font Latin_5x6   = new(){ Height = 5, Dimensions = [0x01, 0x1A] };
 
         /// <summary>
-        /// Latin font Height: 32 | Width: 22
+        /// Latin font Height: 7 | Width: 6
         /// </summary>
-        public static readonly Font Latin_7x6   = new(){ Height = 0x07, Dimensions = [0x01, 0x1B] };
+        public static readonly Font Latin_7x6   = new(){ Height = 7, Dimensions = [0x01, 0x1B] };
 
         /// <summary>
-        /// Latin font Height: 32 | Width: 22
+        /// Latin font Height: 9 | Width: 6
         /// </summary>
-        public static readonly Font Latin_9x6   = new(){ Height = 0x09, Dimensions = [0x01, 0x1C] };
+        public static readonly Font Latin_9x6   = new(){ Height = 9, Dimensions = [0x01, 0x1C] };
 
         /// <summary>
-        /// Latin font Height: 32 | Width: 22
+        /// Latin font Height: 11 | Width: 8
         /// </summary>
-        public static readonly Font Latin_11x8  = new(){ Height = 0x11, Dimensions = [0x01, 0x1D] };
+        public static readonly Font Latin_11x8  = new(){ Height = 11, Dimensions = [0x01, 0x1D] };
 
         /// <summary>
-        /// Latin font Height: 32 | Width: 22
+        /// Latin font Height: 16 | Width: 12
         /// </summary>
-        public static readonly Font Latin_16x12 = new(){ Height = 0x16, Dimensions = [0x01, 0x1E] };
+        public static readonly Font Latin_16x12 = new(){ Height = 16, Dimensions = [0x01, 0x1E] };
 
         /// <summary>
-        /// Latin font Height: 32 | Width: 22
+        /// Latin font Height: 24 | Width: 21
         /// </summary>
-        public static readonly Font Latin_24x21 = new(){ Height = 0x24, Dimensions = [0x01, 0x1F] };
+        public static readonly Font Latin_24x21 = new(){ Height = 24, Dimensions = [0x01, 0x1F] };
     }
 
     public class Font
@@ -43,7 +43,7 @@
         /// <summary>
         /// The height of the character
         /// </summary>
-        public byte Height { get; set; }
+        public int Height { get; set; }
 
         /// <summary>
         /// The dimension that the printer expecting
